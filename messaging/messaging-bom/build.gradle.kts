165891b9de20fdf3415b59f9373899fbd9d7551b
awais <macheema3004@gmail.com>
2026-08-16 00:51:08 +0200

    Add release guard for remote publication

--- messaging/messaging-bom/build.gradle.kts
+++ messaging/messaging-bom/build.gradle.kts
@@ -4,13 +4,31 @@ plugins {
 }
 
 dependencies {
-    constraints { api(project(":messaging:messaging-core"))
+    constraints {
+        api(project(":messaging:messaging-core"))
         api(project(":messaging:messaging-spring"))
         api(project(":messaging:messaging-rabbitmq"))
     }
 }
 
 publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri(
+                "https://maven.pkg.github.com/" +
+                        providers.gradleProperty("siloverse.publish.repository")
+                            .orElse(providers.environmentVariable("GITHUB_REPOSITORY"))
+                            .getOrElse("siloverse/java-library")
+            )
+            credentials {
+                username = providers.gradleProperty("gpr.user")
+                    .orElse(providers.environmentVariable("GITHUB_ACTOR")).orNull
+                password = providers.gradleProperty("gpr.key")
+                    .orElse(providers.environmentVariable("GITHUB_TOKEN")).orNull
+            }
+        }
+    }
     publications {
         create<MavenPublication>("mavenPlatform") {
             from(components["javaPlatform"])
